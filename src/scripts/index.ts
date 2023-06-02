10ab6ad688b91bb5073a836c04e18b7fd87f5320
Matus Tatarko <matus.tatarko28@gmail.com>
2023-06-02 12:56:49 +0200

    feat: adds first home section of portfolio website

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,3 +1,5 @@
+import ScrollReveal from "scrollreveal";
+
 const header = document.querySelector("header");
 
 // ================== Stick Navigation Bar ======================
@@ -9,3 +11,13 @@ const stickNavBar = (): void => {
 stickNavBar();
 
 window.addEventListener("scroll", stickNavBar);
+
+// ================== ScrollReveal animation ======================
+
+const sr = ScrollReveal({
+  duration: 3000,
+  distance: "80px",
+});
+
+sr.reveal(".developer-info", { origin: "bottom", delay: 500 });
+sr.reveal(".developer-image", { origin: "top", delay: 500 });
